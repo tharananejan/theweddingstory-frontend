6bbad25cd259079111ccbd38dfddb8391115298b
Athukorala A.T.N <nejantharana2004.pvt@gmail.com>
2026-03-24 10:12:21 +0530

    Router added and pages created.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to The Wedding Story</h1>
+      <p>Your one-stop destination for all your wedding content creation needs. We specialize in capturing the magic of your special day through stunning photography, videography, and storytelling. Let us help you create lasting memories that you can cherish forever.</p>
+    </div>
+  )
+}
+
+export default Home;
