137bdde5df5db817af0cb6c2fe56c3f910db0bbf
Sahan Dewasinghe <157863707+sahandewasinghe@users.noreply.github.com>
2026-03-25 16:34:40 +0530

    services.jsx page upadated

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,14 @@
+import PageHeader   from './PageHeader';
+import PricingCards from './PricingCards';
+import TrustSection from './TrustSection';
+import styles       from './services.module.css';
 
-function Services() {
+export default function Services() {
   return (
-    <div className="services">
-      <h1>Our Services</h1>
-      <p>At The Wedding Story, we offer a range of services to make your wedding day unforgettable. Our team of experienced photographers and videographers will work closely with you to capture every precious moment. We also provide storytelling services to create a beautiful narrative of your wedding day. Contact us to learn more about our packages and how we can help you create lasting memories.</p>
-    </div>
-  )
+    <main className={styles.page}>
+      <PageHeader />
+      <PricingCards />
+      <TrustSection />
+    </main>
+  );
 }
-
-export default Services;
