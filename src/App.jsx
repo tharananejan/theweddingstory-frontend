6c41a2e5fc52feef0eeb8b55cdde12de90d21421
Athukorala A.T.N <nejantharana2004.pvt@gmail.com>
2026-04-02 10:20:48 +0530

    compatibility-v

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/Header/Footer'
 function App() {
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Header />
       <main style={{ paddingTop: '7rem', paddingBottom: '5rem' }}>
         <Routes>
