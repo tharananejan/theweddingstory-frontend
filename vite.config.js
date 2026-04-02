7f2e1a85da9526924530248a29ce5a1497972333
Athukorala A.T.N <nejantharana2004.pvt@gmail.com>
2026-04-02 10:10:06 +0530

    changed the vite.config.js to be compatible with github pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build';
-  
-  return {
-    plugins: [react()],
-    // Only use the repo name as base when building for production
-    // base: isProduction ? '/theweddingstory-frontend/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
 })
